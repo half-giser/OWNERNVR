a16f901045f7e1a67a042be7031699846a092370
kaho <a11558@tvt.net.cn>
2024-08-08 15:38:22 +0800

    【需求单号】：BA-4340 【需求描述】：NVR Web基于Vue3重构 【修改方案】：基于Vue3重构回放页面 【关联影响点】：无 【是否涉及其他端修改】：设备端 【验证步骤和结果】：登录-》功能面板-》回放，检查页面布局和各功能

---
+++ src/views/UI_PUBLIC/page/playback/PlaybackEventPanel.vue
@@ -0,0 +1,201 @@
+<!--
+ * @Author: yejiahao yejiahao@tvt.net.cn
+ * @Date: 2024-07-30 14:08:41
+ * @Description: 回放-事件类型视图
+ * @LastEditors: yejiahao yejiahao@tvt.net.cn
+ * @LastEditTime: 2024-08-06 20:13:56
+-->
+<template>
+    <fieldset>
+        <legend>{{ Translate('IDCS_RECORD_TYPE') }}</legend>
+        <div class="list">
+            <div
+                v-for="item in pageData.events[pageData.eventIndex]"
+                v-show="item.enable"
+                :key="item.value"
+                class="item"
+                @click="changeEvent(item.value)"
+            >
+                <el-tooltip
+                    :content="item.name"
+                    :show-after="500"
+                >
+                    <BaseImgSprite
+                        :file="pageData.eventList.includes(item.value) ? item.checked : item.unchecked"
+                        :index="0"
+                        :chunk="4"
+                    />
+                </el-tooltip>
+            </div>
+
+            <el-popover
+                v-model:visible="pageData.isEventPop"
+                placement="right"
+                width="420px"
+                trigger="click"
+                popper-class="playback-event"
+                :hide-after="0"
+                :show-after="0"
+            >
+                <template #reference>
+                    <BaseImgSprite
+                        v-show="pageData.isEventPopBtn"
+                        file="event_type_menu"
+                        :chunk="4"
+                        :hover-index="1"
+                        class="btn"
+                    />
+                </template>
+                <div class="event-title">{{ Translate('IDCS_MODE_SELECT') }}</div>
+                <div class="event-list">
+                    <div
+                        v-for="(item, index) in pageData.events"
+                        :key="index"
+                        :class="{ active: index === pageData.activeEventIndex }"
+                        @click="pageData.activeEventIndex = index"
+                    >
+                        <div
+                            v-for="event in item"
+                            v-show="event.enablePop"
+                            :key="event.value"
+                        >
+                            <BaseImgSprite :file="event.file" />
+                            <p>{{ event.name }}</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="event-info">
+                    <h3>{{ Translate('IDCS_DESCRIPTION') }}</h3>
+                    <p
+                        v-for="(tip, key) in pageData.eventTips"
+                        :key
+                    >
+                        {{ tip }}
+                    </p>
+                </div>
+                <el-row>
+                    <el-col
+                        :span="24"
+                        class="el-col-flex-end"
+                    >
+                        <el-button
+                            class="event-btn"
+                            @click="changeEventList"
+                            >{{ Translate('IDCS_OK') }}</el-button
+                        >
+                        <el-button
+                            class="event-btn"
+                            @click="closeEventPop"
+                            >{{ Translate('IDCS_CLOSE') }}</el-button
+                        >
+                    </el-col>
+                </el-row>
+            </el-popover>
+        </div>
+    </fieldset>
+</template>
+
+<script lang="ts" src="./PlaybackEventPanel.v.ts"></script>
+
+<style lang="scss" scoped>
+fieldset {
+    flex-shrink: 0;
+    border: 1px solid var(--border-color7);
+    margin: 15px 15px;
+}
+
+.list {
+    display: flex;
+    justify-content: center;
+    padding: 5px 10px 0 0;
+    position: relative;
+}
+
+.item {
+    margin: 0 5px;
+}
+
+.btn {
+    position: absolute;
+    right: -5px;
+    cursor: pointer;
+}
+
+.event {
+    padding: 0;
+    padding-bottom: 10px;
+
+    &-title {
+        width: 100%;
+        background-color: var(--bg-table-thead);
+        text-align: center;
+        line-height: 30px;
+    }
+
+    &-list {
+        & > div {
+            display: flex;
+            padding: 10px 0;
+            cursor: pointer;
+
+            &:hover {
+                background-color: var(--primary--01);
+            }
+
+            &.active {
+                background-color: var(--primary--04);
+
+                p {
+                    color: var(--color-white);
+                }
+            }
+
+            & > div {
+                width: 100px;
+                display: flex;
+                flex-direction: column;
+                justify-content: center;
+                align-items: center;
+            }
+        }
+
+        p {
+            margin: 0;
+            padding: 0;
+            margin-top: 5px;
+            width: 100%;
+            text-align: center;
+            font-size: 12px;
+        }
+    }
+
+    &-info {
+        margin-bottom: 20px;
+
+        h3 {
+            margin: 10px;
+            font-size: 14px;
+            font-weight: normal;
+        }
+
+        p {
+            margin: 10px;
+            padding: 0;
+            // margin-bottom: 10px;
+            // width: 100%;
+        }
+    }
+
+    &-btn {
+        margin-right: 10px;
+        margin-left: 0;
+    }
+}
+</style>
+
+<style>
+.playback-event {
+    padding: 0 !important;
+    padding-bottom: 10px !important;
+}
+</style>
---
+++ src/views/UI_PUBLIC/page/playback/PlaybackScreenPanel.v.ts
@@ -0,0 +1,419 @@
+/*
+ * @Author: yejiahao yejiahao@tvt.net.cn
+ * @Date: 2024-08-06 20:38:08
+ * @Description: 回放-底部控制视图
+ * @LastEditors: yejiahao yejiahao@tvt.net.cn
+ * @LastEditTime: 2024-08-07 14:16:09
+ */
+import { LiveSharedWinData } from '@/types/apiType/live'
+
+export default defineComponent({
+    props: {
+        /**
+         * @property 当前窗口数据
+         */
+        winData: {
+            type: Object as PropType<LiveSharedWinData>,
+            required: true,
+            default: () => new LiveSharedWinData(),
+        },
+        /**
+         * @property 播放模式
+         */
+        mode: {
+            type: String,
+            required: true,
+            default: '',
+        },
+        /**
+         * @property 当前分屏数
+         */
+        split: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        /**
+         * @property 是否开启OSD状态
+         */
+        osd: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        /**
+         * @property 是否开启POS状态
+         */
+        pos: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        /**
+         * @property 是否开启水印状态
+         */
+        watermark: {
+            type: Boolean,
+            required: true,
+            default: false,
+        },
+        /**
+         * @property 剪切范围
+         */
+        clipRange: {
+            type: Array as PropType<number[]>,
+            required: true,
+            default: () => [],
+        },
+        /**
+         * @property 播放通道列表
+         */
+        playingList: {
+            type: Number,
+            required: true,
+            default: 0,
+        },
+        /**
+         * @property 播放状态
+         */
+        playStatus: {
+            type: String,
+            required: true,
+            default: 'stop',
+        },
+    },
+    emits: {
+        'update:split': (num: number, type: number) => {
+            return typeof num === 'number' && typeof type === 'number'
+        },
+        'update:osd': (bool: boolean) => {
+            return typeof bool === 'boolean'
+        },
+        'update:pos': (bool: boolean) => {
+            return typeof bool === 'boolean'
+        },
+        'update:watermark': (bool: boolean) => {
+            return typeof bool === 'boolean'
+        },
+        fullscreen() {
+            return true
+        },
+        stop() {
+            return true
+        },
+        pause() {
+            return true
+        },
+        pauseBackwards() {
+            return true
+        },
+        nextFrame() {
+            return true
+        },
+        prevFrame() {
+            return true
+        },
+        jump(seconds: number) {
+            return typeof seconds === 'number'
+        },
+        resume() {
+            return true
+        },
+        backwards() {
+            return true
+        },
+        setSpeed(speed: number) {
+            return typeof speed === 'number'
+        },
+        pos(bool: boolean) {
+            return typeof bool === 'boolean'
+        },
+        clipStart() {
+            return true
+        },
+        clipEnd() {
+            return true
+        },
+        backUp() {
+            return true
+        },
+    },
+    setup(prop, ctx) {
+        const { Translate } = useLangStore()
+        const systemCaps = useCababilityStore()
+
+        const WASM_SEG = [1, 4].map((split) => ({
+            split,
+            type: 1,
+        }))
+
+        const OCX_SEG = [1, 4, 9, 16, 25, 36]
+            .filter((item) => {
+                return item <= systemCaps.playbackMaxWin
+            })
+            .map((split) => ({
+                split,
+                type: 1,
+            }))
+
+        const pageData = ref({
+            // H5模式分屏
+            wasmSeg: WASM_SEG,
+            // OCX模式分屏
+            ocxSeg: OCX_SEG,
+            // 播放可配置速度列表
+            speedList: [1 / 32, 1 / 16, 1 / 8, 1 / 4, 1 / 2, 1, 2, 4, 8, 16, 32],
+            // 速度值索引
+            speedIndex: 5,
+        })
+
+        // 没有播放视图时，禁用操作按钮
+        const disabled = computed(() => {
+            return prop.playingList <= 0
+        })
+
+        /**
+         * @description 暂停播放
+         */
+        const pause = () => {
+            if (disabled.value) {
+                return
+            }
+            if (prop.playStatus === 'backwards') {
+                ctx.emit('pauseBackwards')
+            } else {
+                ctx.emit('pause')
+            }
+        }
+
+        /**
+         * @description 停止播放
+         */
+        const stop = () => {
+            if (disabled.value) {
+                return
+            }
+            ctx.emit('stop')
+        }
+
+        /**
+         * @description 恢复播放
+         */
+        const resume = () => {
+            if (disabled.value) {
+                return
+            }
+            ctx.emit('resume')
+        }
+
+        /**
+         * @description 倒放
+         */
+        const backwards = () => {
+            if (disabled.value) {
+                return
+            }
+            ctx.emit('backwards')
+        }
+
+        // 当前速度值
+        const speed = computed(() => {
+            return pageData.value.speedList[pageData.value.speedIndex]
+        })
+
+        // 禁用慢速
+        const rewindDisabled = computed(() => {
+            return pageData.value.speedIndex === 0 || disabled.value
+        })
+
+        /**
+         * @description 设置速度. H5模式和OCX模式表示慢速的速度值有差异
+         */
+        const setSpeed = () => {
+            if (prop.mode === 'h5') {
+                ctx.emit('setSpeed', speed.value)
+            } else if (prop.mode === 'ocx') {
+                if (speed.value < 1) {
+                    ctx.emit('setSpeed', (1 / speed.value) * -1)
+                } else {
+                    ctx.emit('setSpeed', speed.value)
+                }
+            }
+        }
+
+        /**
+         * @description 慢速
+         */
+        const rewind = () => {
+            if (rewindDisabled.value) {
+                return
+            }
+            pageData.value.speedIndex--
+            setSpeed()
+        }
+
+        // 禁用倍速
+        const forwardDisabled = computed(() => {
+            return pageData.value.speedIndex === pageData.value.speedList.length - 1 || disabled.value
+        })
+
+        /**
+         * @description 倍速
+         */
+        const forward = () => {
+            if (forwardDisabled.value) {
+                return
+            }
+            pageData.value.speedIndex++
+            setSpeed()
+        }
+
+        // 禁用重置倍速播放
+        const resetSpeedDisabled = computed(() => {
+            return speed.value === 1 || disabled.value
+        })
+
+        /**
+         * @description 重置倍速播放
+         */
+        const resetSpeed = () => {
+            if (resetSpeedDisabled.value) {
+                return
+            }
+            pageData.value.speedIndex = pageData.value.speedList.findIndex((index) => index === 1)
+            setSpeed()
+        }
+
+        // 显示播放速度
+        const displaySpeed = computed(() => {
+            return speed.value < 1 ? `X1/${1 / speed.value}` : `X${speed.value}`
+        })
+
+        watch(
+            () => prop.playStatus,
+            (newVal, oldVal) => {
+                if (newVal === 'play' && oldVal !== 'play') {
+                    ctx.emit('setSpeed', speed.value)
+                }
+            },
+        )
+
+        /**
+         * @description 跳转播放
+         * @param {number} seconds 单位：秒
+         */
+        const jump = (seconds: number) => {
+            if (disabled.value) {
+                return
+            }
+            ctx.emit('jump', seconds)
+        }
+
+        // description 是否禁用切换帧
+        const nextFrameDisabled = computed(() => {
+            return disabled.value || prop.playStatus !== 'pause'
+        })
+
+        /**
+         * @description 下一帧
+         */
+        const nextFrame = () => {
+            if (nextFrameDisabled.value) {
+                return
+            }
+            ctx.emit('nextFrame')
+        }
+
+        /**
+         * @description 上一帧
+         */
+        const prevFrame = () => {
+            if (nextFrameDisabled.value) {
+                return
+            }
+            ctx.emit('prevFrame')
+        }
+
+        // 是否禁用POS
+        const posDisabled = computed(() => {
+            return !systemCaps.supportPOS || disabled.value
+        })
+
+        /**
+         * @description 开启/关闭POS
+         * @param {Boolean} bool
+         */
+        const togglePos = (bool: boolean) => {
+            if (posDisabled.value) {
+                return
+            }
+            ctx.emit('update:pos', bool)
+        }
+
+        /**
+         * @description 设置备份开始点
+         */
+        const setClipStart = () => {
+            if (disabled.value) {
+                return
+            }
+            ctx.emit('clipStart')
+        }
+
+        /**
+         * @description 设置备份结束点
+         */
+        const setClipEnd = () => {
+            if (disabled.value) {
+                return
+            }
+            ctx.emit('clipEnd')
+        }
+
+        // 禁用备份按钮
+        const backUpDisabled = computed(() => {
+            return disabled.value || prop.clipRange.length !== 2 || prop.clipRange[0] === prop.clipRange[1]
+        })
+
+        /**
+         * @description 备份
+         */
+        const backUp = () => {
+            if (disabled.value) {
+                return
+            }
+            if (backUpDisabled.value) {
+                ElMessage.info(Translate('IDCS_SELECT_BACKUP_START_END_TIME'))
+                return
+            }
+            ctx.emit('backUp')
+        }
+
+        return {
+            speed,
+            pageData,
+            disabled,
+            stop,
+            resume,
+            backwards,
+            pause,
+            forward,
+            rewind,
+            resetSpeed,
+            resetSpeedDisabled,
+            forwardDisabled,
+            rewindDisabled,
+            jump,
+            nextFrameDisabled,
+            nextFrame,
+            prevFrame,
+            posDisabled,
+            togglePos,
+            displaySpeed,
+            setClipStart,
+            setClipEnd,
+            backUpDisabled,
+            backUp,
+        }
+    },
+})
