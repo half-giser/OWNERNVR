396dbbd4847afb11c77522e07c8338ecb7030527
A11600 <A11600@tvt.net.cn>
2024-08-12 10:02:27 +0800

    【需求单号】：BA-4340 【需求描述】：NVR Web基于Vue3重构 【修改方案】：录像码流表格组件以及事件录像码流、定时录像码流两个页面 【关联影响点】：无 【是否涉及其他端修改】：无 【验证步骤和结果】：

--- src/api/rec.ts
+++ src/api/rec.ts
@@ -2,8 +2,8 @@
  * @Author: yejiahao yejiahao@tvt.net.cn
  * @Date: 2024-07-17 17:02:54
  * @Description: 录像与回放
- * @LastEditors: yejiahao yejiahao@tvt.net.cn
- * @LastEditTime: 2024-07-29 18:24:19
+ * @LastEditors: gaoxuefeng gaoxuefeng@tvt.net.cn
+ * @LastEditTime: 2024-08-06 17:09:41
  */
 import http, { getXmlWrapData } from './api'
 
@@ -22,7 +22,7 @@ export const editManualRecord = (data: string) => http.fetch('editManualRecord',
 export const queryManualRecord = (data: string) => http.fetch('queryManualRecord', getXmlWrapData(data))
 
 /**
- * @description 获取节点码流信息
+ * @description 获取节点信息
  * @param {string} data
  * @returns
  */
@@ -34,3 +34,24 @@ export const queryNodeEncodeInfo = (data: string) => http.fetch('queryNodeEncode
  * @returns
  */
 export const editNodeEncodeInfo = (data: string) => http.fetch('editNodeEncodeInfo', getXmlWrapData(data))
+
+/**
+ * @description 获取录像分配信息
+ * @param
+ * @returns
+ */
+export const queryRecordDistributeInfo = () => http.fetch('queryRecordDistributeInfo', getXmlWrapData(''))
+
+/**
+ * @description 获取系统宽带容量
+ * @param
+ * @returns
+ * */
+export const querySystemCaps = () => http.fetch('querySystemCaps', getXmlWrapData(''))
+
+/**
+ * @description 获取录像剩余时间
+ * @param {string} data
+ * @returns
+ */
+export const queryRemainRecTime = (data: string) => http.fetch('queryRemainRecTime', getXmlWrapData(data))
