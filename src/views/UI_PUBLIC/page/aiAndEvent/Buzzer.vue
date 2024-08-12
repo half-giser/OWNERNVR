22a4423394159745c00bd74f87bd53f024c43c12
lym <a11593@tvt.net.cn>
2024-08-12 18:10:06 +0800

    【需求单号】：BA-4340 【需求描述】：NVR Web基于Vue3重构 【修改方案】：事件通知——显示、蜂鸣器、推送 【关联影响点】：无 【是否涉及其他端修改】：无 【验证步骤和结果】：无

---
+++ src/views/UI_PUBLIC/page/aiAndEvent/Buzzer.vue
@@ -0,0 +1,48 @@
+<!--
+ * @Description: AI/事件——事件通知——蜂鸣器
+ * @Author: luoyiming luoyiming@tvt.net.cn
+ * @Date: 2024-07-30 15:55:28
+ * @LastEditors: luoyiming luoyiming@tvt.net.cn
+ * @LastEditTime: 2024-08-12 15:16:17
+-->
+<template>
+    <el-form
+        ref="popVideoRef"
+        :model="formData"
+        class="stripe"
+        :style="{
+            '--form-input-width': '215px',
+        }"
+        label-position="left"
+        inline-message
+    >
+        <el-form-item
+            prop="popVideoDuration"
+            :label="Translate('IDCS_DELAY')"
+        >
+            <el-select v-model="formData.buzzerDuration">
+                <el-option
+                    v-for="value in pageData.buzzerDurationOption"
+                    :key="value"
+                    :value="value"
+                    :label="displayDurationOption(value)"
+                >
+                </el-option>
+            </el-select>
+        </el-form-item>
+        <div
+            class="base-btn-box"
+            :style="{ paddingTop: '10px' }"
+        >
+            <el-button
+                class="btn-ok"
+                @click="setData"
+                >{{ Translate('IDCS_APPLY') }}</el-button
+            >
+        </div>
+    </el-form>
+</template>
+
+<script lang="ts" src="./Buzzer.v.ts"></script>
+
+<style scoped></style>
---
+++ src/views/UI_PUBLIC/page/aiAndEvent/Push.v.ts
@@ -0,0 +1,107 @@
+/*
+ * @Description: AI/事件——事件通知——推送
+ * @Author: luoyiming luoyiming@tvt.net.cn
+ * @Date: 2024-08-12 15:28:16
+ * @LastEditors: luoyiming luoyiming@tvt.net.cn
+ * @LastEditTime: 2024-08-12 17:14:36
+ */
+import { pushForm } from '@/types/apiType/aiAndEvent'
+import ScheduleManagPop from '../../components/schedule/ScheduleManagPop.vue'
+
+export default defineComponent({
+    components: {
+        ScheduleManagPop,
+    },
+    setup() {
+        const { Translate } = useLangStore()
+        const { openMessageTipBox } = useMessageBox()
+        const { openLoading, closeLoading, LoadingTarget } = useLoading()
+        const pushFormData = ref(new pushForm())
+
+        const pageData = ref({
+            scheduleOption: [] as SelectOption<string, string>[],
+            //排程管理弹窗显示状态
+            scheduleManagPopOpen: false,
+        })
+
+        const getScheduleData = async () => {
+            const result = await queryScheduleList()
+
+            commLoadResponseHandler(result, ($) => {
+                pageData.value.scheduleOption = $('/response/content/item').map((item) => {
+                    return {
+                        value: item.attr('id') as string,
+                        label: item.text(),
+                    }
+                })
+            })
+        }
+
+        const getData = async () => {
+            await getScheduleData()
+            const result = await queryEventNotifyParam()
+
+            commLoadResponseHandler(result, ($) => {
+                pushFormData.value.chkEnable = $('/response/content/mobilePushSwitch').text() == 'true'
+
+                if ($('/response/content/pushSchedule').attr('id')) {
+                    pushFormData.value.pushSchedule = $('/response/content/pushSchedule').attr('id')
+                } else {
+                    const scheduleName = $('/response/content/pushSchedule').text() || '24x7'
+                    pageData.value.scheduleOption.forEach((item) => {
+                        if (scheduleName == item.label) {
+                            pushFormData.value.pushSchedule = item.value
+                        }
+                    })
+                }
+            })
+        }
+
+        // 测试按钮，向手机发送测试推送信息
+        const testMobile = async () => {
+            const result = await testMobilePush()
+            const $ = queryXml(result)
+            if ($('/response/status').text() == 'success') {
+                openMessageTipBox({
+                    type: 'success',
+                    title: Translate('IDCS_SUCCESS_TIP'),
+                    message: Translate('IDCS_TEST_PUSH_MOBILE_SUCCESS'),
+                    showCancelButton: false,
+                })
+            } else {
+                openMessageTipBox({
+                    type: 'info',
+                    title: Translate('IDCS_INFO_TIP'),
+                    message: Translate('IDCS_TEST_PUSH_MOBILE_FAILED'),
+                    showCancelButton: false,
+                })
+            }
+        }
+
+        const setData = async () => {
+            const sendXml = rawXml`
+                <content>
+                    <mobilePushSwitch>${String(pushFormData.value.chkEnable)}</mobilePushSwitch>
+                    <pushSchedule id='${pushFormData.value.pushSchedule}'></pushSchedule>
+                </content>
+            `
+            const result = await editEventNotifyParam(sendXml)
+            commSaveResponseHadler(result)
+        }
+
+        onMounted(async () => {
+            openLoading(LoadingTarget.FullScreen)
+
+            await getData()
+
+            closeLoading(LoadingTarget.FullScreen)
+        })
+
+        return {
+            pushFormData,
+            pageData,
+            testMobile,
+            setData,
+        }
+    },
+})
